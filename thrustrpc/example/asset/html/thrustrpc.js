4508dc1422bc5072c69cc95fdbc6df2eaa05c6ef
UD00005 <xqp@udantech.com>
2016-12-01 20:02:49 +0800

    refector

--- thrustrpc/example/asset/html/thrustrpc.js
+++ thrustrpc/example/asset/html/thrustrpc.js
@@ -3,40 +3,37 @@ var thrustHandlers = {};
 var thrustCallSeq = 0;
 
 function thisMsgHandler(event) {
-  var msg = JSON.parse(event.payload);
-  console.log(msg);
-  var dir = msg['dir'];
-  var seq = msg['seq'];
-  if (dir == 'reply') {
-    var err = msg['err']
-    var _reply = msg['data'];
-    var call = thrustPendingCalls[seq];
-    if (call == undefined) {
-      return;
-    }
-    clearTimeout(call['timer']);
-    if (call['cbk'] != undefined) {
-	  var reply = JSON.parse(_reply);
-      call['cbk'](reply, err);
-    }
-    return
-  }
+    var msg = JSON.parse(event.payload);
+    console.log(msg);
+    var dir = msg['dir'];
+    var seq = msg['seq'];
 
-  if (dir == "call") {
-    var ret = {
-        dir: "reply",
-        seq: seq,
-    };
-
-    var method = msg['method'];
-    var fn = thrustPendingCalls[method]
-    if (fn == undefined){
-        ret['err'] = "Unknown method"
-    } else {
-        ret['data'] = JSON.stringify(fn(msg['data']))
+    if (dir == 'reply') {
+        var err = msg['err']
+        var call = thrustPendingCalls[seq];
+        if (call != undefined) {
+            clearTimeout(call['timer']);
+            if (call['cbk'] != undefined) {
+                var reply = undefined;
+                if (err != undefined) {
+                    reply = JSON.parse(msg['data']);
+                }
+                call['cbk'](reply, err);
+            }
+        }
+        return
     }
-    THRUST.remote.send(JSON.stringify(ret));
-  }
+
+    if (dir == "call") {
+        var ret = {dir: "reply", seq: seq};
+        var fn = thrustPendingCalls[msg['method']]
+        if (fn == undefined){
+            ret['err'] = "Unknown method"
+        } else {
+            ret['data'] = JSON.stringify(fn(msg['data']))
+        }
+        THRUST.remote.send(JSON.stringify(ret));
+    }
 }
 
 function thrustRpcRegister(method, fn) {
@@ -44,28 +41,24 @@ function thrustRpcRegister(method, fn) {
 }
 
 function thrustRpcTimeout(seq) {
-  var call = thrustPendingCalls(seq);
-  if (call != undefined) {
-    thrustPendingCalls.delete(seq);
-    if (call['cbk'] != undefined) {
-        call['cbk'].cbk(undefined, "timeout");
+    var call = thrustPendingCalls(seq);
+    if (call != undefined) {
+        thrustPendingCalls.delete(seq);
+        if (call['cbk'] != undefined) {
+            call['cbk'].cbk(undefined, "timeout");
+        }
     }
-  }
 }
 
 function thrustRpcCall(method, arg, cbk, timeout) {
-    var call = {
-        cbk:cbk,
-    }
-    var msg = {
+    var call = { cbk:cbk }
+
+    THRUST.remote.send(JSON.stringify({
         dir: "call",
         seq: thrustCallSeq,
         data: JSON.stringify(arg),
-        method: method,
-    };
-    THRUST.remote.send(JSON.stringify(msg));
-    var timer = setTimeout(thrustRpcTimeout, timeout, thrustCallSeq);
-    call['timer'] = timer;
+        method: method}));
+    call['timer'] = setTimeout(thrustRpcTimeout, timeout, thrustCallSeq);
     thrustPendingCalls[thrustCallSeq] = call;
     thrustCallSeq++;
 }
